e756c84be7772424829e1f593a4abb4bd0301847
Raghav10026 <raghav10026@gmail.com>
2021-01-09 01:29:45 +0530

    bootstrap added

--- React App/confusion/src/App.js
+++ React App/confusion/src/App.js
@@ -1,26 +1,20 @@
 import logo from './dp1.jpg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
     return ( <
         div className = "App" >
         <
-        header className = "App-header" >
+        Navbar dark color = "primary" >
         <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
+        div className = "container" >
         <
-        p >
-        Edit < code > src / App.js < /code> and save to reload. < /
-        p > <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        I am Raghav <
-        /a> < /
-        header > <
+        NavbarBrand href = "/" > Raghav Nagpal < /NavbarBrand> < /
+        div > <
+        /Navbar>
+
+        <
         /div>
     );
 }
